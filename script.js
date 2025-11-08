9025a25f2bbe595d3bd084ff0da74d01041552dc
HoualefAkram <akram1232004@gmail.com>
2025-11-08 22:18:54 +0100

    added JS translation

---
+++ script.js
@@ -0,0 +1,88 @@
+class Node {
+  constructor( rep, freq, left=null, right=null) {
+        this.rep = rep;
+        this.freq = freq;
+        this.left = left;
+        this.right = right;
+  }
+
+    toString() {
+    return `(${this.rep}, ${this.freq}, left: ${this.left}, right: ${this.right})`;
+    }
+}
+
+class Tree {
+    constructor(nodes) {
+        this.nodes = nodes;
+    }
+
+    toString() {
+    return this.nodes.map(node => String(node)).join("\n");
+    }
+
+  sort() {
+    this.nodes.sort((a, b) => a.freq - b.freq);
+  }
+
+  isMinimal() {
+    return this.nodes.length === 1;
+  }
+
+  combineLastTwo() {
+    const n1 = this.nodes[0];
+    const n2 = this.nodes[1];
+
+    this.nodes = this.nodes.filter(n => n !== n1 && n !== n2);
+
+    this.nodes.push(
+      new Node(`${n1.rep}-${n2.rep}`, n1.freq + n2.freq, n1, n2)
+    );
+  }
+}
+
+
+function frequency(text) {
+  const result = {};
+  for (const l of text) {
+    if (result[l]) {
+      result[l] += 1;
+    } else {
+      result[l] = 1;
+    }
+  }
+  return result;
+}
+
+
+ function dfsPreorder(root, currentStr = "", code = {}) {
+  if (root.left) {
+    dfsPreorder(root.left, currentStr + "0", code);
+  }
+
+  if (root.right) {
+    dfsPreorder(root.right, currentStr + "1", code);
+  }
+
+  if (!root.left && !root.right) {
+    code[root.rep] = currentStr;
+  }
+  return code;
+}
+
+
+const text = "AABCADDEEEF";
+const freq = frequency(text);
+const nodes = Object.entries(freq).map(([key, value]) => new Node(key, value));
+const tree = new Tree();
+tree.nodes = nodes;
+tree.sort();
+
+while (!tree.isMinimal()) {
+  tree.combineLastTwo();
+  tree.sort();
+}
+
+const code = dfsPreorder(tree.nodes[0]);
+console.log(code);
+
+
