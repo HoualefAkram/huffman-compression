24b8c6b2a71ec011bb3a1af61e6e8e5020630d2f
HoualefAkram <akram1232004@gmail.com>
2025-11-09 01:49:25 +0100

    added 1 letter edge case

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 class Node {
-  constructor( rep, freq, left=null, right=null) {
-        this.rep = rep;
-        this.freq = freq;
-        this.left = left;
-        this.right = right;
+  constructor(rep, freq, left = null, right = null) {
+    this.rep = rep;
+    this.freq = freq;
+    this.left = left;
+    this.right = right;
   }
 
-    toString() {
+  toString() {
     return `(${this.rep}, ${this.freq}, left: ${this.left}, right: ${this.right})`;
-    }
+  }
 }
 
 class Tree {
-    constructor(nodes) {
-        this.nodes = nodes;
-    }
+  constructor(nodes) {
+    this.nodes = nodes;
+  }
 
-    toString() {
+  toString() {
     return this.nodes.map(node => String(node)).join("\n");
-    }
+  }
 
   sort() {
     this.nodes.sort((a, b) => a.freq - b.freq);
@@ -53,8 +53,12 @@ function frequency(text) {
   return result;
 }
 
+function compressionRate(originalLength, compressedLength) {
+  return (originalLength - compressedLength) / originalLength;
+}
 
- function dfsPreorder(root, currentStr = "", code = {}) {
+
+function dfsPreorder(root, currentStr = "", code = {}) {
   if (root.left) {
     dfsPreorder(root.left, currentStr + "0", code);
   }
@@ -64,21 +68,21 @@ function frequency(text) {
   }
 
   if (!root.left && !root.right) {
-    code[root.rep] = currentStr;
+    code[root.rep] = currentStr === "" ? "0" : currentStr;
   }
   return code;
 }
 
-function buildUl(root, currentUl=document.createElement("ul")) {
+function buildUl(root, currentUl = document.createElement("ul")) {
   let li = document.createElement("li");
   li.textContent = root.rep;
   currentUl.appendChild(li);
-  if(root.left) {
+  if (root.left) {
     let newUl = document.createElement("ul");
     li.appendChild(newUl);
     buildUl(root.left, newUl)
   }
-  if(root.right) {
+  if (root.right) {
     let newUl = document.createElement("ul");
     li.appendChild(newUl);
     buildUl(root.right, newUl)
@@ -88,53 +92,61 @@ function buildUl(root, currentUl=document.createElement("ul")) {
 
 
 function encode(text, codeMap) {
-    c = "";
-    for (const t of text) {
-        c = c + codeMap[t];
-    }
-    return c;
+  c = "";
+  for (const t of text) {
+    c = c + codeMap[t];
+  }
+  return c;
 }
 
 
 function generate() {
-    let text = document.getElementById("text").value;
+  let text = document.getElementById("text").value;
 
-    let displayCode = document.getElementById("code");
-    let table = document.getElementById("table");
-    let graph = document.getElementById("graph");
+  let displayCode = document.getElementById("code");
+  let displayRate = document.getElementById("rate");
+  let table = document.getElementById("table");
+  let graph = document.getElementById("graph");
 
-    displayCode.textContent = "";
-    table.innerHTML = ""; 
-    graph.innerHTML = "";  
+  displayCode.textContent = "";
+  table.innerHTML = "";
+  graph.innerHTML = "";
+  displayRate.innerHTML = "";
 
-    const freq = frequency(text);
-    const nodes = Object.entries(freq).map(([key, value]) => new Node(key, value));
-    const tree = new Tree();
-    tree.nodes = nodes;
-    tree.sort();
+  const freq = frequency(text);
+  const nodes = Object.entries(freq).map(([key, value]) => new Node(key, value));
+  const tree = new Tree();
+  tree.nodes = nodes;
+  tree.sort();
 
-    while (!tree.isMinimal()) {
+  while (!tree.isMinimal()) {
     tree.combineLastTwo();
     tree.sort();
-    }
+  }
 
-    const codeMap = dfsPreorder(tree.nodes[0]);
-    const code = encode(text,codeMap);  
-    displayCode.textContent = `code: ${code}`;
+  const codeMap = dfsPreorder(tree.nodes[0]);
+  const code = encode(text, codeMap);
+
+  const originalBits = text.length * 8;
+  const compressedBits = code.length;
+  const rate = compressionRate(originalBits, compressedBits)
+
+  displayCode.textContent = `code: ${code}`;
+  displayRate.textContent = `Original bits: ${originalBits} bits | Compressed bits: ${compressedBits} | Compression Rate: ${parseInt(rate * 100)} %`
 
 
-    var i = 0;
-    for (const [key, value] of Object.entries(codeMap)) {
-        let row = table.insertRow(i);
-        let col1 = row.insertCell(0);
-        col1.textContent = key;
-        let col2 = row.insertCell(1);
-        col2.textContent = value;
-        i=i+1;
-    }
-    // draw the graph
-    const treeUl = buildUl(tree.nodes[0]);
-    graph.appendChild(treeUl);
+  var i = 0;
+  for (const [key, value] of Object.entries(codeMap)) {
+    let row = table.insertRow(i);
+    let col1 = row.insertCell(0);
+    col1.textContent = key;
+    let col2 = row.insertCell(1);
+    col2.textContent = value;
+    i = i + 1;
+  }
+  // draw the graph
+  const treeUl = buildUl(tree.nodes[0]);
+  graph.appendChild(treeUl);
 }
 
 
